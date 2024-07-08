97bda58e22bec33a85f2dea294845da9ad56ff43
Michael Kargl <michael.kargl@posteo.net>
2024-07-08 18:11:38 +0200

    feat: adds quadratic equationn support

---
+++ src/components/calculation.tsx
@@ -0,0 +1,77 @@
+import {ComputeEngine} from "@cortex-js/compute-engine";
+import TeX from '@matejmazur/react-katex';
+import React from 'react';
+
+type Variables = {
+    [variable: string]: number
+}
+
+interface VariableListProps {
+    variables: Variables
+}
+
+const VariableList: React.FC<VariableListProps> = (props) => (
+    <pre>
+        Variables: <br/>{
+        Object.entries(props.variables).map(entry => <>
+            <span>{entry[0]}: {entry[1]}</span><br/>
+        </>)
+    }</pre>
+);
+
+export interface CalculationProps {
+    katexEquation: string,
+    fractionDigits?: number,
+    debug?: boolean,
+    variables: Variables;
+}
+
+export const Calculation: React.FC<CalculationProps> = (props) => {
+    if (!props.katexEquation?.length) {
+        return <></>;
+    }
+
+    const separator = <>&nbsp;&nbsp; = &nbsp;&nbsp;</>;
+    const mathEngine = new ComputeEngine({
+        numericPrecision: props.fractionDigits
+    });
+    mathEngine.pushScope();
+
+    const expression = mathEngine.parse(props.katexEquation, {
+        canonical: undefined
+    });
+
+    // NOTE: most of these steps are unnecessary but demonstrate nicely some capabilities of cortexjs
+
+    const symbolicEvaluatedExpression = expression.evaluate({numericMode: false});
+    const symbolicEvaluatedExpresionSubstituted = symbolicEvaluatedExpression.subs(props.variables);
+
+    const simplifiedSymbolicEvaluatedExpression = symbolicEvaluatedExpression.simplify();
+    const simplifiedSymbolicEvaluatedExpressionSubstituted = simplifiedSymbolicEvaluatedExpression.subs(props.variables);
+
+    const simplestNumericExpression = symbolicEvaluatedExpresionSubstituted.simplify();
+
+    const equationFunc = simplifiedSymbolicEvaluatedExpression.compile();
+
+    const rawResult: number = equationFunc(props.variables);
+    const result = rawResult.toFixed(props.fractionDigits);
+
+    return (<>
+        <VariableList variables={props.variables}/>
+        <TeX math={props.katexEquation}/>
+        {separator}
+        <TeX math={expression.latex}/>
+        {separator}
+        <TeX math={symbolicEvaluatedExpression.latex}/>
+        {separator}
+        <TeX math={simplifiedSymbolicEvaluatedExpression.latex}/>
+        {separator}
+        <TeX math={simplifiedSymbolicEvaluatedExpressionSubstituted.latex}/>
+        {separator}
+        <TeX math={simplestNumericExpression.latex}/>
+        {separator}
+        <TeX math={rawResult.toString()}/>
+        <br/>{separator}
+        <TeX math={`${result} \\checkmark`}/>
+    </>);
+}
--- src/components/equation.tsx
+++ src/components/equation.tsx
@@ -1,70 +1,40 @@
+import React from "react";
 import {ComputeEngine} from "@cortex-js/compute-engine";
-import TeX from '@matejmazur/react-katex';
-import React from 'react';
+import TeX from "@matejmazur/react-katex";
+import { CalculationProps } from './calculation'
 
-type Variables = {
-    [variable: string]: number
-}
-
-interface VariableListProps {
-    variables: Variables
-}
-
-const VariableList: React.FC<VariableListProps> = (props) => (
-    <pre>
-        Variables: <br/>{
-        Object.entries(props.variables).map(entry => <>
-            <span>{entry[0]}: {entry[1]}</span><br/>
-        </>)
-    }</pre>
-);
-
-interface EquationProps {
-    katexEquation: string,
-    fractionDigits?: number,
-    debug?: boolean,
-    variables: Variables;
+interface EquationProps extends CalculationProps {
+    solveFor: string
 }
 
 export const Equation: React.FC<EquationProps> = (props) => {
-    const separator = <>&nbsp;&nbsp; = &nbsp;&nbsp;</>;
+    if (!props.katexEquation?.length) {
+        return <></>;
+    }
+
     const mathEngine = new ComputeEngine({
         numericPrecision: props.fractionDigits
     });
+
+    const originalEquation = mathEngine.parse(props.katexEquation);
+
     mathEngine.pushScope();
-
-    const expression = mathEngine.parse(props.katexEquation, {
-        canonical: undefined
-    });
-    const symbolicEvaluatedExpression = expression.evaluate({numericMode: false});
-    const symbolicEvaluatedExpresionSubstituted = symbolicEvaluatedExpression.subs(props.variables);
-
-    const simplifiedSymbolicEvaluatedExpression = symbolicEvaluatedExpression.simplify();
-    const simplifiedSymbolicEvaluatedExpressionSubstituted = simplifiedSymbolicEvaluatedExpression.subs(props.variables);
-
-    const simplestNumericExpression = symbolicEvaluatedExpresionSubstituted.simplify();
-
-    const equationFunc = simplifiedSymbolicEvaluatedExpression.compile();
-
-    const rawResult: number = equationFunc(props.variables);
-    const result = rawResult.toFixed(props.fractionDigits);
+    mathEngine.assign(props.variables);
+    const solvedEquations = originalEquation.solve(props.solveFor);
+    const filledEquation = originalEquation.subs(props.variables);
 
     return (<>
-        <VariableList variables={props.variables}/>
-        <TeX math={props.katexEquation}/>
-        {separator}
-        <TeX math={expression.latex}/>
-        {separator}
-        <TeX math={symbolicEvaluatedExpression.latex}/>
-        {separator}
-        <TeX math={simplifiedSymbolicEvaluatedExpression.latex}/>
-        {separator}
-        <TeX math={simplifiedSymbolicEvaluatedExpressionSubstituted.latex}/>
-        {separator}
-        <TeX math={simplestNumericExpression.latex}/>
-        {separator}
-        <TeX math={rawResult.toString()} />
-        <br/>{separator}
-        <TeX math={`${result} \\checkmark`} />
+        <TeX math={originalEquation.latex}/>
+    <br/>
+    <TeX math={filledEquation.latex}/>
+    <hr/>
+    {solvedEquations.map(e => (<>
+        <TeX>{`${props.solveFor} =`}</TeX>
+        <TeX math={e.latex}/>
+        <TeX> = </TeX>
+        <TeX math={e.evaluate().N().numericValue.toString()} />
+    <TeX>\checkmark</TeX>
+    <br/>
+    </>))}
     </>);
 }
